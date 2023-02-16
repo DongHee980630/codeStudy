dfb7439a849f209e1a387883d4b1cbf27a7e4470
DongHee980630 <ehdehdrnt5@gmail.com>
2023-02-16 12:30:14 +0900

    객체지향 연습

--- algorithm/CodeStudy.java
+++ algorithm/CodeStudy.java
@@ -1,4 +1,4 @@
-package src;
+package algorithm;
 
 
 import java.util.Arrays;
---
+++ objectStudy/objectEx1_1.java
@@ -0,0 +1,26 @@
+package objectStudy;
+
+class objectEx1_1 {
+    public static void main(String[] args) {
+        Tv t;
+        t = new Tv();
+        t.channel = 7;
+        t.channelDown();
+        System.out.println(t.channel);
+    }
+}
+class Tv {
+    String color;
+    boolean power;
+    int channel;
+    void power() {
+        power = !power;
+    }
+    void channelUp() {
+        ++channel;
+    }
+    void channelDown() {
+        --channel;
+    }
+}
+
