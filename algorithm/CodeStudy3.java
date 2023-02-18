ecd73a320f289437c18275804bc672d94b591b5c
DongHee980630 <ehdehdrnt5@gmail.com>
2023-02-18 13:26:19 +0900

    알고리즘 day4

--- algorithm/CodeStudy3.java
+++ algorithm/CodeStudy3.java
@@ -1,6 +1,15 @@
 package algorithm;
 
+import com.sun.tools.javac.Main;
+
 import java.util.Arrays;
+class CodeStudy3 {
+    public  static void main(String[] args) {
+        Solution28 method = new Solution28();
+        System.out.println(method.solution(11));
+
+    }
+}
 
 //이상한 문자 만들기
 class Solution21 {
@@ -64,6 +73,77 @@ class Solution24 {
             ans += arr[i];
         }
         return Long.parseLong(ans);
+    }
+}
+
+//정수 제곱근 판별
+class Solution25 {
+    public long solution(long n) {
+        long answer = 0;
+        for(int i =0; i *i <=n;i++){
+            if(i *i ==n){
+                answer = (i+1) * (i+1);
+            }
+        }
+        return answer == 0? -1:answer;
+    }
+}
+
+//제일 작은 수 제거하기
+class Solution26 {
+    public int[] solution(int[] arr) {
+        if(arr.length ==1){
+            int[] answer ={-1};
+            return answer;
+        }
+        int[] answer = new int[arr.length - 1];
+        int m = arr[0];
+        for (int i = 0; i < arr.length;i++){
+            m = Math.min(m,arr[i]);
+        }
+        int a = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==m){
+                continue;
+            }
+            answer[a++] = arr[i];
+        }
+        return answer;
+    }
+}
+class Solution27 {
+    public int solution(int n) {
+        int answer = 0;
+        long num = n;
+        while (num != 1){
+            if (num % 2 ==0){
+                num /= 2;
+            }else {
+                num = 3 * num +1;
+            }answer++;
+            if (answer > 500){
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
+
+//하샤드 수
+class Solution28 {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int a = x;
+        while(a > 0){
+            sum += a%10;
+            a /= 10;
+        }
+        if(x % sum == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
